cc03b018f6b93aa91c7f18b836c8c828fd515e92
Idan G <idangozlan@gmail.com>
2015-10-26 15:00:37 +0200

    Cron Actions fixes
    Version mistakenly changed to 1.0.5 instead of 1.0.4

--- zray/WordpressCrons.php
+++ zray/WordpressCrons.php
@@ -5,16 +5,21 @@ namespace ZRayWordpress;
 class WordpressCrons {
 	
 	public function beforeExit($context, &$storage) {
+		return $storage['crons'] = $this->getCrons();
+	}
+    
+    public function getCrons(){
+        $crons_arr = array ();
 		$schedules = array ();
 		foreach ( wp_get_schedules () as $key => $schedule ) {
 			$schedules [$key] = $schedule ['display'];
 		}
-		
-		if (is_array ( _get_cron_array () )) {
+        if (is_array ( _get_cron_array () )) {
 			foreach ( _get_cron_array () as $time => $crons ) {
 				foreach ( $crons as $name => $cron ) {
 					foreach ( $cron as $subcron ) {
-						$storage['crons'] [] = array (
+                        
+						$crons_arr [] = array (
 								'hook' => $name,
 								'schedule' => empty ( $schedules [$subcron ['schedule']] ) ? $subcron ['schedule'] : $schedules [$subcron ['schedule']],
 								'nextExecution' => human_time_diff ( $time ) . (time () > $time ? ' ago' : ''),
@@ -25,7 +30,8 @@ class WordpressCrons {
 				}
 			}
 		}
-	}
+        return $crons_arr;
+    }
 	
 }
 
