de5a6bbf9a153c2a9a40a6784d4ee894711cbf60
Idan G <idangozlan@gmail.com>
2015-10-26 10:36:37 +0200

    Crons: + Fixed time sorting
    Hooks:
    + Fixed Core sorting after refresh
    
    Versioning:
    + 1.0.4 => 1.0.5

--- zray/WordpressCrons.php
+++ zray/WordpressCrons.php
@@ -5,12 +5,7 @@ namespace ZRayWordpress;
 class WordpressCrons {
 	
 	public function beforeExit($context, &$storage) {
-		$storage['crons'] = $this->getCrons();
-	}
-    
-    public function getCrons(){
-        $schedules = array ();
-        $crons_arr = array ();
+		$schedules = array ();
 		foreach ( wp_get_schedules () as $key => $schedule ) {
 			$schedules [$key] = $schedule ['display'];
 		}
@@ -19,18 +14,18 @@ class WordpressCrons {
 			foreach ( _get_cron_array () as $time => $crons ) {
 				foreach ( $crons as $name => $cron ) {
 					foreach ( $cron as $subcron ) {
-						$crons_arr [] = array (
+						$storage['crons'] [] = array (
 								'hook' => $name,
 								'schedule' => empty ( $schedules [$subcron ['schedule']] ) ? $subcron ['schedule'] : $schedules [$subcron ['schedule']],
 								'nextExecution' => human_time_diff ( $time ) . (time () > $time ? ' ago' : ''),
+                                'nextExecutionTimestamp' => $time,
 								'arguments' => count ( $subcron ['args'] ) > 0 ? print_r ( $subcron ['args'], true ) : '' 
 						);
 					}
 				}
 			}
 		}
-        return $crons_arr;
-    }
+	}
 	
 }
 
